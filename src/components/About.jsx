f7dba5008ef72a5a260a091b455e938af445f9e8
salilaherath <salilaherath@gmail.com>
2023-05-05 11:11:32 +0530

    About Us

---
+++ src/components/About.jsx
@@ -0,0 +1,26 @@
+const About = () => {
+	return (
+		<div className=" mt-8 w-full flex flex-col items-center xl:mt-20">
+			<div className="w-4/5 ">
+				<h1 className="text-2xl text-center font-semibold md:text-4xl xl:text-6xl">
+					ABOUT US
+				</h1>
+				<p className="text-black text-base text-justify md:text-lg mt-4 xl:mt-12 lg:text-xl xl:text-2xl">
+					KAMEcho Solutions (Private) Limited is a Sri Lankan based company
+					which provides diversified management solutions for global companies.
+					Our services include Management consultations for start-ups, Marketing
+					solutions, Web based solutions, Online stores and a trading platform.
+				</p>
+				<p className="text-black text-base text-justify md:text-lg mt-4 xl:mt-12 lg:text-xl xl:text-2xl">
+					We were established on 25th of August 2021 with a team of experts in
+					IT, Marketing, HR and Strategic Management. With the expansion of the
+					project portfolio, we started to get advise from industry experts to
+					ensure that we provide solutions that serve beyond customer
+					satisfaction.
+				</p>
+			</div>
+		</div>
+	);
+};
+
+export default About;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,20 +4,20 @@ const Hero = () => {
 	return (
 		<div>
 			{/* <div className="md:h-[80vh] w-full"> */}
-			<div className="bg-black h-[30vh] w-full relative md:h-[50vh] xl:h-[70vh]">
+			<div className="bg-black h-[30vh] w-full relative md:h-[40vh] xl:h-[60vh]">
 				<img
 					className="h-full w-full object-cover absolute object-center"
 					src={HeroImg}
 					alt="Hero Image"
 				/>
 				<div className="backdrop-brightness-75 w-full h-full grid place-items-center">
-					<div className="absolute top-[40%] flex flex-col w-4/5 place-items-center text-center">
+					<div className="absolute top-[35%] flex flex-col w-4/5 place-items-center text-center">
 						<h1 className="text-lg text-white font-semibold drop-shadow-md md:text-4xl xl:text-6xl">
-							Welcome to KAMEcho Solutions!
+							Welcome to
 						</h1>
-						<h3 className="text-xs mt-2 text-white drop-shadow-md md:text-xl xl:text-3xl">
-							We work hard to provide the solution you need
-						</h3>
+						<h1 className="text-2xl text-white font-semibold drop-shadow-md md:text-5xl xl:text-8xl">
+							KAMEcho Solutions!
+						</h1>
 					</div>
 				</div>
 			</div>
